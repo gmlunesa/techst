e1e1b7fd50c6a79ea3f5ae012411a2b93721b5c0
gmlunesa <gmlunesa@gmail.com>
2022-12-12 15:51:45 +0800

    Finalized pages and functionalities

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import React from "react";
+import Sidebar from "../components/Sidebar";
+import Chat from "../components/Chat";
+
+import { HomeContainer, HomeChatWrapper } from "../components/ui/Home";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <HomeContainer>
+      <HomeChatWrapper>
+        <Sidebar />
+        <Chat />
+      </HomeChatWrapper>
+    </HomeContainer>
+  );
 };
 
 export default Home;
